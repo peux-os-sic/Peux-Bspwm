d1a0ab693f237b7ff93c85a387379a88e2e56586
DN-debug <75267055+DN-debug@users.noreply.github.com>
2021-04-29 16:46:36 +0530

    commit

--- install.sh
+++ install.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
+
+##################################################
+## Author - DN-debug
+## Description - Peux Bspwm setup via fetcher app
+##################################################
+
 # Install packages
 
 if zenity --question --text="Install Bspwm?"
 then 
-    PASSWD="$(zenity --password --title=Authentication)\n"
-    echo -e $PASSWD | sudo -S pacman -Sy bspwm sxhkd
-    notify-send "installed Bspwm"
 
+    PASSWD="$(zenity --password --title=Authentication)\n"
+
+    pkg="$(pacman -Qq | grep bspwm)"
+
+    if [ "${pkg}" = "bspwm" ]; then
+        notify-send "Bspwm is already installed. Skipping!"   
+    else 
+        echo -e $PASSWD | sudo -S pacman -Syy bspwm sxhkd
+        notify-send "installed Bspwm"
+    fi
     if zenity --question --text="Install Tint2 Panel?"
     then 
-        PASSWD="$(zenity --password --title=Authentication)\n"
-        echo -e $PASSWD | sudo -S pacman -Sy tint2
-        notify-send "installed Tint2"
-    else
-        notify-send "skipping Tint2 installation!"
+        pkg1="$(pacman -Qq | grep tint2)"
+        if [ "${pkg1}" = "tint2" ]; then
+            notify-send "Tint2 is already installed. Skipping!"
+        else
+            echo -e $PASSWD | sudo -S pacman -Syy tint2
+            notify-send "installed Tint2"
+        fi
     fi
 
 else
     notify-send "skipping Bspwm installation!"
 fi
 
-if zenity --question --text="Want to copy the config?"
+# copy configurations
+
+if zenity --question --text="Want to copy the configurations?"
 then 
     echo "changing directory"
     cd ../
@@ -31,5 +48,7 @@ else
     notify-send "skipping the setup!"
 fi
 
-rm -rf /tmp/fetcher/Peux-Bspwm
+# cleanup
+
+rm -rf /tmp/fetcher/Peux-Bpswm
 notify-send "Done!"
